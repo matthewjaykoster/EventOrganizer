6bcb9f58e21dd77395eaefc2cdff15de7b3c005b
clemenger <antisnollygoster@gmail.com>
2013-06-17 19:57:36 -0400

    Installed express and routes

---
+++ Node/app.js
@@ -0,0 +1,24 @@
+
+
+var express = require('express');
+var routes = require('routes')
+var http = require('http');
+
+var app = express();
+
+app.configure(function(){
+              app.set('port', process.env.PORT || 3000);
+
+             
+//              app.use(express.static(__dirname + '/public'));
+
+
+});
+
+
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+                              console.log("Express server listening on port " + app.get('port'));
+                              });
